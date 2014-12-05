5cee8f31cc67c5f8fedb879648971e70b6cef514
Samuele Santi <redshadow@hackzine.org>
2014-12-05 16:25:16 +0100

    Added demo function

--- ckan_crawl_demo/__init__.py
+++ ckan_crawl_demo/__init__.py
@@ -1,2 +1,24 @@
-def download_and_print_ckan_datasets():
-    pass
+import logging
+
+from ckan_api_client.high_level import CkanHighlevelClient
+from jobcontrol.globals import current_app
+
+
+def download_and_print_ckan_datasets(ckan_url):
+    """
+    Download and print datasets from ckan
+    """
+
+    client = CkanHighlevelClient(ckan_url)
+
+    logger = logging.getLogger('ckan_crawl_demo')
+    logger.info('Starting function')
+
+    total = len(client.list_datasets())
+    current_app.report_progress(None, 0, total)
+
+    for cnt, dataset in enumerate(client.iter_datasets()):
+        logger.debug(repr(dataset))
+        current_app.report_progress(None, cnt + 1, total)
+
+    return total
